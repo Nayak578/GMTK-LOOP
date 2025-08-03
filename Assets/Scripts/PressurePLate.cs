524944a2e09b262b02793c1941aa9bf77c4baa28
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-08-03 11:24:28 +0530

    Bug Fixes in Pressure Plate

--- Assets/Scripts/PressurePLate.cs
+++ Assets/Scripts/PressurePLate.cs
@@ -1,39 +1,58 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class PressurePLate : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    Renderer rend;
-    void Start()
-    {
+public class PressurePLate : MonoBehaviour {
+    private Renderer rend;
+    private HashSet<Collider> activeColliders = new HashSet<Collider>();
+
+    void Start() {
         rend = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    private void OnTriggerEnter(Collider other) {
-        rend.material.color = Color.green;
-        MoveObstacle();
-        rend.material.EnableKeyword("_EMISSION");
-        rend.material.SetColor("_EmissionColor", Color.green *7f);
-    }
-    private void OnTriggerExit(Collider other) {
-        BlockObstacle();
-        rend.material.color = Color.red;
-        rend.material.SetColor("_EmissionColor", Color.red * 7f);
+    void Update() {
+        // Clean up any destroyed objects
+        activeColliders.RemoveWhere(c => c == null|| !IsColliderStillInside(c));
 
+        if (activeColliders.Count == 0) {
+            SetInactive();
+        }
     }
+
+    private void OnTriggerEnter(Collider other) {
+        activeColliders.Add(other);
+        SetActive();
+        MoveObstacle();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        activeColliders.Remove(other);
+        if (activeColliders.Count == 0) {
+            SetInactive();
+            BlockObstacle();
+        }
+    }
+    bool IsColliderStillInside(Collider col) {
+        // Checks if collider is still inside this trigger's bounds
+        if (!col.gameObject.activeInHierarchy) return false;
+
+        return GetComponent<Collider>().bounds.Intersects(col.bounds);
+    }
+    void SetActive() {
+        rend.material.color = Color.green;
+        rend.material.EnableKeyword("_EMISSION");
+        rend.material.SetColor("_EmissionColor", Color.green * 7f);
+    }
+
+    void SetInactive() {
+        rend.material.color = Color.red;
+        rend.material.SetColor("_EmissionColor", Color.red*7f);
+    }
+
     void MoveObstacle() {
         Debug.Log("Move Obstacle");
-        //use lerp to show moving in update set current and final pos
-        //set final and current pos here
     }
+
     void BlockObstacle() {
         Debug.Log("BlockWithObstacle");
-
     }
 }
